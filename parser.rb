f23c4d86700fca9923d4cda4850703a8b261a347
Alan O'Donnell <alan.m.odonnell@gmail.com>
2013-12-08 19:18:58 -0500

    Cosmetic cleanup

--- parser.rb
+++ parser.rb
@@ -3,6 +3,17 @@ require 'treetop'
 Treetop.load 'arithmetic'
 
 # This is where the difference AST type cases go.
+#
+# (define-type ArithC
+#   [numC (n : number)]
+#   [plusC (l : ArithC) (r : ArithC)]
+#   [multC (l : ArithC) (r : ArithC)])
+
+class NumC < Treetop::Runtime::SyntaxNode
+  def n
+    text_value.to_i
+  end
+end
 
 class PlusC < Treetop::Runtime::SyntaxNode
   # def l
@@ -22,14 +33,8 @@ class MultC < Treetop::Runtime::SyntaxNode
   # end
 end
 
-class NumC < Treetop::Runtime::SyntaxNode
-  def n
-    text_value.to_i
-  end
-end
 
-# This is a small wrapper around the stuff that TreeTop provides
-# automatically.
+# This is a small wrapper around what TreeTop provides automatically.
 module Parser
   # string -> NumC(n) | PlusC(l, r) | MultC(l, r) | nil
   def self.parse(s)
